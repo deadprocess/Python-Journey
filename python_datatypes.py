a138cb632b43cbcc5f657c275b96b1fbcf46103b
Alex <56584652+deadprocess@users.noreply.github.com>
2021-01-24 20:32:47 +0100

    first commit

---
+++ python_datatypes.py
@@ -0,0 +1,45 @@
+#Python Datatypes
+
+#That's a string.
+a = str("Hello world")
+print(a)
+
+#That's an int.
+b = int(20)
+print(b)
+
+#That's a float.
+c = float(20.5)
+print(c)
+
+#That's a complex.
+d = complex(1j)
+print(d)
+
+#That's a list.
+e = list(("apple", "banana", "cherry"))
+print(e)
+
+#That's a tuple.
+f = tuple(("apple", "banana", "cherry"))
+print(f)
+
+#That's a range.
+g = range(6)
+print(g)
+
+#That's a dict.
+h = dict(name="John", age=36)
+print(h)
+
+#That's a set.
+i = set(("apple", "banana", "cherry"))
+print(i)
+
+#That's a frozenset.
+j = frozenset(("apple", "banana", "cherry"))
+print(j)
+
+# That's a Bool
+k = bool(5)
+print(k)
