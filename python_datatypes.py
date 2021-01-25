994085b06048093c342ddc8a931c0c3cad936363
Alex <56584652+deadprocess@users.noreply.github.com>
2021-01-25 19:38:26 +0100

    added more datatypes

--- python_datatypes.py
+++ python_datatypes.py
@@ -1,45 +1,58 @@
-#Python Datatypes
+# Python Datatypes
 
-#That's a string.
+# That's a string.
 a = str("Hello world")
 print(a)
 
-#That's an int.
+# That's an int.
 b = int(20)
 print(b)
 
-#That's a float.
+# That's a float.
 c = float(20.5)
 print(c)
 
-#That's a complex.
+# That's a complex.
 d = complex(1j)
 print(d)
 
-#That's a list.
+# That's a list.
 e = list(("apple", "banana", "cherry"))
 print(e)
 
-#That's a tuple.
+# That's a tuple.
 f = tuple(("apple", "banana", "cherry"))
 print(f)
 
-#That's a range.
+# That's a range.
 g = range(6)
 print(g)
 
-#That's a dict.
+# That's a dict.
 h = dict(name="John", age=36)
 print(h)
 
-#That's a set.
+# That's a set.
 i = set(("apple", "banana", "cherry"))
 print(i)
 
-#That's a frozenset.
+# That's a frozenset.
 j = frozenset(("apple", "banana", "cherry"))
 print(j)
 
-# That's a Bool
+# That's a Bool.
 k = bool(5)
 print(k)
+
+# That are bytes.
+el = bytes(5)
+print(el)
+
+# That's a byte-array
+m = bytearray(5)
+print(5)
+
+# That's a memory-view
+n = memoryview(bytes(5))
+print(n)
+
