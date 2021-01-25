293f904e746bc2d578d1da4b5ce2677b13f65cfb
Alex <56584652+deadprocess@users.noreply.github.com>
2021-01-25 19:56:03 +0100

    minor changes

--- python_datatypes.py
+++ python_datatypes.py
@@ -1,26 +1,38 @@
 # Python Datatypes
 
 # That's a string.
+a = "Hello world"
+# Setting the Specific Data Type
 a = str("Hello world")
 print(a)
 
 # That's an int.
+b = 20
+# Setting the Specific Data Type
 b = int(20)
 print(b)
 
 # That's a float.
+c = 20.5
+# Setting the Specific Data Type
 c = float(20.5)
 print(c)
 
 # That's a complex.
+d = 1j
+# Setting the Specific Data Type
 d = complex(1j)
 print(d)
 
 # That's a list.
+e = ["apple", "banana", "cherry"]
+# Setting the Specific Data Type
 e = list(("apple", "banana", "cherry"))
 print(e)
 
 # That's a tuple.
+f = ("apple", "banana", "cherry")
+# Setting the Specific Data Type
 f = tuple(("apple", "banana", "cherry"))
 print(f)
 
@@ -29,22 +41,32 @@ g = range(6)
 print(g)
 
 # That's a dict.
+h = {"name": "John", "age": 36}
+# Setting the Specific Data Type
 h = dict(name="John", age=36)
 print(h)
 
 # That's a set.
+i = {"apple", "banana", "cherry"}
+# Setting the Specific Data Type
 i = set(("apple", "banana", "cherry"))
 print(i)
 
 # That's a frozenset.
+j = frozenset({"apple", "banana", "cherry"})
+# Setting the Specific Data Type
 j = frozenset(("apple", "banana", "cherry"))
 print(j)
 
 # That's a Bool.
+k = True
+# Setting the Specific Data Type
 k = bool(5)
 print(k)
 
 # That are bytes.
+el = b"hello"
+# Setting the Specific Data Type
 el = bytes(5)
 print(el)
 
@@ -53,5 +75,6 @@ m = bytearray(5)
 print(5)
 
 # That's a memory-view
+# Setting the Specific Data Type
 n = memoryview(bytes(5))
 print(n)
